6bc3afc74d19d8abab646518af83387e947fe1d7
juliusbacosa <juliusbacosa@gmail.com>
2017-03-12 23:03:23 +0800

    Initial

---
+++ WCFSERVICELIBRARY/HelloWorldService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFSERVICELIBRARY
+{
+
+    [DataContract]
+    public class Name
+    {
+        [DataMember]
+        public string FirstName;
+        [DataMember]
+        public string LastName;
+             
+    }
+
+    [ServiceContract]
+    public interface IHelloWorld
+    {
+        [OperationContract]
+        string SayHello(Name person);
+    }
+
+    public class HelloWorldService : IHelloWorld
+    {
+        public string SayHello(Name person)
+        {
+            return string.Format("Hello {0} {1}", person.FirstName, person.LastName);
+        }
+    }
+}
